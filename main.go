0f3250cb7292c057173a0a731b5f247a1517b028
Elísio Mualumene <85412882+elisiomualumene@users.noreply.github.com>
2023-03-11 16:28:12 +0000

    :fire: firsts errors

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import "net/http"
+import "fmt"
 
 type User struct {
 	id String
@@ -9,14 +10,16 @@ type User struct {
 }
 
 func main(){
+	user := User{
+		id: "1234",
+		name: "Elisio Mualumene",
+		created_at: "11/03/2023",
+	}
+	println(user)
 	http.HandleFunc("/", HomeHandle)
 	http.ListenAndServe(":3000",nil)
 }
 
 func HomeHandle(w http.ResponseWriter, r *http.Request){
 	w.Write([]byte("Hello World"))
-}
-
-func UserHandle(w, http.ResponseWriter, r *http.Request){
-	
 }
